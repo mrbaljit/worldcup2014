40f650db9ce93dc6c9eca4f39507d9300c6cadac
Baljit Singh <baljit_singh@vero.co.nz>
2014-09-30 14:24:20 +1300

    Initial import

--- src/main/java/co/fifa/world/cup/MainController.java
+++ src/main/java/co/fifa/world/cup/MainController.java
@@ -18,55 +18,17 @@ public class MainController {
 
 	protected static Logger logger = Logger.getLogger("controller");
 	
-	public static final String SOCCER = "soccer";
-	
 	@Resource(name="personService")
 	private PersonService personService;
 	
-	/**
-	 * Handles and retrieves all persons and show it in a JSP page
-	 * 
-	 * @return the name of the JSP page
-	 */
-    @RequestMapping(value = "/bett", method = RequestMethod.GET)
-    public String getPersons(Model model) {
-    	logger.debug("Received request to show all bets");
-    	
-    	// Retrieve all persons by delegating the call to PersonService
-    	List<Person> persons = personService.getAll();
-    	
-    	// Attach persons to the Model
-    	model.addAttribute("persons", persons);
-    	model.addAttribute("totalAmount", persons.get(0).getTotalAmount());
-    	
-    	// This will resolve to /WEB-INF/jsp/personspage.jsp
-    	return "personspage";
-	}
-    
-    /**
-     * Retrieves coinsurance details given Policy Id.
-     * @param policyId
-     * @param model
-     * @return
-     
-    @RequestMapping(method = RequestMethod.GET, value = "bets.html")
-    public String openCoinsurance(Model model) {
-
-        System.err.println(">>22222222222222 >>>>>>>>>>>>>>>>>>>>>> ");
-
-        return SOCCER;
-    } */   
-    
     @RequestMapping(method = RequestMethod.GET, value = "getSoccer.json")
     public @ResponseBody
-    PersonJSONBean getPremiumHistory()
+    PersonJSONBean getSoccer()
     {
-        
         PersonJSONBean jsonBean = new PersonJSONBean();
         List<Person> persons = personService.getAll();
         jsonBean.setBets(persons);
         jsonBean.setTotalAmount(persons.get(0).getTotalAmount().toString());
-        System.err.println("returning >>>>>>>>>>>>>>>>>>>>>>>>> ");
         return jsonBean;
     }
     
